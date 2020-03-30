92f7d62b4ce1fdc0e4fce31778543f29b16a66a9
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-03-30 15:49:48 +0900

    add other page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 
+// 自分のjs
+const index = require('./index');
+
+// ミドルウェアを渡す
+app.use('/index', index);
 // パスにアクセスされるたびに呼び出されるコールバック関数を定義する
 app.get('/', function (req, res) {
     // レスポンスを返す
